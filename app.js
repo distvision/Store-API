26cb9bf37a494c25285a70fd3e038e355f364f7d
distvision <furede2@gmail.com>
2022-07-17 20:52:42 +0200

    setting .env routes and controllers

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('04 Store API')
+require("dotenv").config();
+// async erros
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middlewrare
+app.use(express.json());
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products route</a>');
+});
+
+app.use("/api/v1/products", productsRouter);
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+// server
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening to ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
